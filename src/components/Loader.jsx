c1735064f33e0590c8503349771edd481ea7ca18
Harshal Katiyar <161059815+harshal-katiyar@users.noreply.github.com>
2025-09-27 12:41:21 +0530

    Update2

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,13 @@
-// import React from 'react'
+import React from 'react'
 // import { BiLoaderAlt } from 'react-icons/bi'
 
 
-// export const Loader = () => {
-//   return (
-//     <div className='flex justify-center  items-center pt-44'>
-//       {/* <BiLoaderAlt className='animate-spin  font-bold' size={59} /> */}
-//       <span class="loader"></span>
-//     </div>
-//   )
-// }
+export const Loader = () => {
+  return (
+    <div className='flex justify-center  items-center pt-44'>
+      <span ></span>
+    </div>
+  )
+}
+
 
